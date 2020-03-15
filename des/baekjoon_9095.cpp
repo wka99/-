01a0a43d974b0ac2fa679e5ab044b5ab18bd1d5b
wka99 <yooso020599@naver.com>
2020-03-15 20:10:11 +0900

    03/15(2-1)

--- des/baekjoon_9095.cpp
+++ des/baekjoon_9095.cpp
@@ -2,12 +2,22 @@
 #include <vector>
 using namespace std;
 //1, 2, 3 더하기
+
 int result = 0;
+int dpm[11] = { 1,2,4, };
+
 void dp(int n) {
 	if (n == 0)result++;
 	if (n - 1 >= 0) { dp(n - 1); }
 	if (n - 2 >= 0) { dp(n - 2); }
 	if (n - 3 >= 0) { dp(n - 3); }
+}
+int dp2(int n) {
+	if (n == 1) return 1;
+	if (n == 2) return 2;
+	if (n == 3) return 4;
+	if (dpm[n] != 0) return dpm[n];
+	else return dp2(n - 1)+ dp2(n - 2) + dp2(n - 3);
 }
 int main() {
 	int T=0; //테스트 케이스의 개수
@@ -22,5 +32,8 @@ int main() {
 		dp(n[i]);
 		cout << result<<endl;
 		result = 0;
+	}
+	for (int i = 0; i < T; i++) {
+		cout << dp2(n[i]) << endl;
 	}
 }

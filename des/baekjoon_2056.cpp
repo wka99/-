785b82e0bf49c128681eb5ba8c9addffa485db3d
wka99 <yooso020599@naver.com>
2020-07-03 00:37:30 +0900

    07/03

--- des/baekjoon_2056.cpp
+++ des/baekjoon_2056.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <queue>
 using namespace std;
+//위상 정렬
 
 int N;
-int task[10001][101] = { 0, }; //작업간 선행 관계
-int time[101] = { 0, }; //작업별 수행 시간
-int leftw[101] = { 0, }; //작업별 남은 선행 작업들
+vector <int> task[10001]; //작업간 선행 관계
+int time[10001] = { 0, }; //작업별 수행 시간
+int degree[10001] = { 0, }; //작업별 남은 선행 작업 개수 (진입 차수)
 queue <int> first; //선행 관계가 없는 작업들
 int total = 0; //모든 작업을 완료하기 위한 최소 시간
+int d[10001] = { 0, };
 
-void BFS() {
+void topologySort() {
 	for (int i = 1; i <= N; i++) {
-		if (leftw[i] == 0) {
+		if (degree[i] == 0) {
 			first.push(i);
+			d[i] = time[i];
 		}
 	}
-	/*
-	int max = 0;
-	while (!first.empty()) {
-		int f = first.front();
+	for (int i = 1; i <= N; i++) {
+		if (first.empty())
+			break;
+		int x = first.front();
 		first.pop();
-		for (int i = 1; i <= N; i++) {
-			if (task[i][f] == 1) {
-				task[i][f] = 0;
-				leftw[i]--;
+		for (int j = 0; j < task[x].size(); j++) {
+			d[task[x][j]] = max(d[task[x][j]], d[x] + time[task[x][j]]);
+			if (--degree[task[x][j]] == 0) {
+				first.push(task[x][j]);
 			}
 		}
-	}*/
+	}
 }
 int main() {
 	cin >> N;
@@ -35,11 +39,12 @@ int main() {
 	int tmp;
 	for (int i = 1; i <= N; i++) {
 		cin >> time[i] >> j;
-		leftw[i] = j;
+		degree[i] = j;
 		for (; j > 0; j--) {
 			cin >> tmp;
-			task[i][tmp] = 1;
+			task[tmp].push_back(i);
 		}
 	}
-	BFS();
+	topologySort();
+	cout << *max_element(d, d + N + 1) << endl;
 }

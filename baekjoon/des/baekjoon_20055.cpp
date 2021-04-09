2d53550e734b02e3d8f0a2ff180ded700ed512ec
Soyoung Yoo <yooso020599@naver.com>
2021-04-09 23:29:01 +0900

    04/09

---
+++ baekjoon/des/baekjoon_20055.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+#define MAX_N 201
+
+int N, K;
+queue<int> belt;
+int A[MAX_N];
+
+int main() {
+	cin >> N >> K;
+	for (int i = 0; i < N * 2; i++) cin >> A[i];
+
+}
--- baekjoon/des/소스.cpp
+++ baekjoon/des/소스.cpp
@@ -1,93 +1,151 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 using namespace std;
-#define MAX_N 11
+#define MAX_N 21
 
-class INFO {
-public:
-	int rx, ry, bx, by;
-	int cnt = 0;
-};
+int N;
+vector<vector<int>> board(MAX_N, vector<int>(MAX_N, 0));
+int ans = 0;
 
-int N, M;
-INFO start;
-char map[MAX_N][MAX_N];
-int visited[MAX_N][MAX_N][MAX_N][MAX_N];
-int dx[4] = { -1,1,0,0 };
-int dy[4] = { 0,0,-1,1 };
-
-int play() {
-	int ans = -1;
-	queue <INFO> q;
-	q.push(start);
-	visited[start.rx][start.ry][start.bx][start.by] = 1;
-	while (!q.empty()) {
-		INFO curr = q.front(); q.pop();
-		if (curr.cnt > 10) break;
-		if (map[curr.rx][curr.ry] == 'O' && map[curr.bx][curr.by] != 'O') {
-			ans = curr.cnt;
-			break;
-		}
-		for (int i = 0; i < 4; i++) {
-			int next_rx = curr.rx;
-			int next_ry = curr.ry;
-			int next_bx = curr.bx;
-			int next_by = curr.by;
-			while (map[next_rx][next_ry] != '#' && map[next_rx][next_ry] != 'O') {
-				next_rx += dx[i];
-				next_ry += dy[i];
+void UP(vector<vector<int>> board, vector<vector<int>>& ret) {
+	for (int i = 0; i < N; i++) {
+		vector<int> tmp;
+		int flag = 0;
+		for (int j = 0; j < N; j++) {
+			if (board[j][i] == 0) continue;
+			if (!flag) {
+				tmp.push_back(board[j][i]);
+				flag = 1;
 			}
-			if (map[next_rx][next_ry] == '#') {
-				next_rx -= dx[i];
-				next_ry -= dy[i];
-			}
-			while (map[next_bx][next_by] != '#' && map[next_bx][next_by] != 'O') {
-				next_bx += dx[i];
-				next_by += dy[i];
-			}
-			if (map[next_bx][next_by] == '#') {
-				next_bx -= dx[i];
-				next_by -= dy[i];
-			}
-			if (next_rx == next_bx && next_ry == next_by) {
-				if (map[next_rx][next_ry] != 'O') {
-					int red = abs(next_rx - curr.rx) + abs(next_ry - curr.ry);
-					int blue = abs(next_bx - curr.bx) + abs(next_by - curr.by);
-					if (blue > red) {
-						next_bx -= dx[i];
-						next_by -= dy[i];
-					}
-					else {
-						next_rx -= dx[i];
-						next_ry -= dy[i];
-					}
+			else {
+				int top = tmp[tmp.size() - 1];
+				if (top == board[j][i]) {
+					tmp.pop_back();
+					tmp.push_back(board[j][i] + top);
+					flag = 0;
+				}
+				else {
+					tmp.push_back(board[j][i]);
 				}
 			}
-			if (visited[next_rx][next_ry][next_bx][next_by]) continue;
-			visited[next_rx][next_ry][next_bx][next_by] = 1;
-			INFO next;
-			next.rx = next_rx; next.ry = next_ry;
-			next.bx = next_bx; next.by = next_by;
-			next.cnt = curr.cnt + 1;
-			q.push(next);
+		}
+		int idx = 0;
+		for (int j = 0; j < tmp.size(); j++) {
+			ret[idx][i] = tmp[j]; idx++;
 		}
 	}
-	return ans;
+}
+void DOWN(vector<vector<int>> board, vector<vector<int>>& ret) {
+	for (int i = 0; i < N; i++) {
+		vector<int> tmp;
+		int flag = 0;
+		for (int j = N-1; j >= 0; j--) {
+			if (board[j][i] == 0) continue;
+			if (!flag) {
+				tmp.push_back(board[j][i]);
+				flag = 1;
+			}
+			else {
+				int top = tmp[tmp.size() - 1];
+				if (top == board[j][i]) {
+					tmp.pop_back();
+					tmp.push_back(board[j][i] + top);
+					flag = 0;
+				}
+				else {
+					tmp.push_back(board[j][i]);
+				}
+			}
+		}
+		int idx = N - 1;
+		for (int j = 0; j < tmp.size(); j++) {
+			ret[idx][i] = tmp[j]; idx--;
+		}
+	}
+}
+void LEFT(vector<vector<int>> board, vector<vector<int>>& ret) {
+	for (int i = 0; i < N; i++) {
+		vector<int> tmp;
+		int flag = 0;
+		for (int j = 0; j < N; j++) {
+			if (board[i][j] == 0) continue;
+			if (!flag) {
+				tmp.push_back(board[i][j]);
+				flag = 1;
+			}
+			else {
+				int top = tmp[tmp.size()-1];
+				if (top == board[i][j]) {
+					tmp.pop_back();
+					tmp.push_back(board[i][j] + top);
+					flag = 0;
+				}
+				else {
+					tmp.push_back(board[i][j]);
+				}
+			}
+		}
+		int idx = 0;
+		for (int j = 0; j < tmp.size(); j++) {
+			ret[i][idx] = tmp[j]; idx++;
+		}
+	}
+}
+void RIGHT(vector<vector<int>> board, vector<vector<int>>& ret) {
+	for (int i = 0; i < N; i++) {
+		vector<int> tmp;
+		int flag = 0;
+		for (int j = N-1; j >=0; j--) {
+			if (board[i][j] == 0) continue;
+			if (!flag) {
+				tmp.push_back(board[i][j]);
+				flag = 1;
+			}
+			else {
+				int top = tmp[tmp.size() - 1];
+				if (top == board[i][j]) {
+					tmp.pop_back();
+					tmp.push_back(board[i][j] + top);
+					flag = 0;
+				}
+				else {
+					tmp.push_back(board[i][j]);
+				}
+			}
+		}
+		int idx = N - 1;
+		for (int j = 0; j < tmp.size(); j++) {
+			ret[i][idx] = tmp[j]; idx--;
+		}
+	}
+}
+void play(int cnt, vector<vector<int>> board) {
+	if (cnt == 5) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				ans = max(ans, board[i][j]);
+			}
+		}
+		return;
+	}
+	for (int i = 0; i < 4; i++) {
+		vector<vector<int>> ret(MAX_N, vector<int>(MAX_N, 0));
+		if (i == 0) UP(board, ret);
+		else if (i == 1) DOWN(board, ret);
+		else if (i == 2) LEFT(board, ret);
+		else if (i == 3) RIGHT(board, ret);
+		play(cnt + 1, ret);
+	}
 }
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	cin >> N >> M;
+	cin >> N;
 	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> map[i][j];
-			if (map[i][j] == 'R') {
-				start.rx = i; start.ry = j;
-			}
-			else if (map[i][j] == 'B') {
-				start.bx = i; start.by = j;
-			}
+		for (int j = 0; j < N; j++) {
+			cin >> board[i][j];
 		}
 	}
-	cout << play() << endl;
+	play(0, board);
+	cout << ans << endl;
 }

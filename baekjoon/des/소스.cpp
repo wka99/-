8b4d9fe5201ad510d8862e05113f3cca1ad7fcee
Soyoung Yoo <yooso020599@naver.com>
2021-04-26 12:16:00 +0900

    04/26

--- baekjoon/des/소스.cpp
+++ baekjoon/des/소스.cpp
@@ -1,21 +1,59 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
 using namespace std;
-#define MAX_N 501
-#define INF 5000001
+#define MAX_N 200001
+#define OFFSET 524288
+#define SZ_TR 1048576
 
-int dist[MAX_N];
-vector<pair<int, int>> grph[MAX_N];
-int N, M;
+int N, H;
+int tree[SZ_TR];
 
+void update(int from, int to) {
+	from += OFFSET;
+	to += OFFSET;
+	while (from <= to) {
+		if (from % 2 == 1) {
+			tree[from]++; from++;
+		}
+		if (to % 2 == 0) {
+			tree[to]++; to--;
+		}
+		from /= 2; to /= 2;
+	}
+}
+int query(int idx) {
+	int ret = 0;
+	idx += OFFSET;
+	ret += tree[idx];
+	idx /= 2;
+	while (idx > 0) {
+		ret += tree[idx];
+		idx /= 2;
+	}
+	return ret;
+}
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	cin >> N >> M;
-	for (int i = 1; i <= N; i++)dist[i] = INF;
-	int a, b, c;
-	for (int i = 0; i < M; i++) {
-		cin >> a >> b >> c;
-		grph[a].push_back({ b,c });
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N >> H;
+	int h;
+	for (int i = 0; i < N; i++) {
+		cin >> h;
+		if (i % 2 == 0) {//석순
+			update(0, h - 1);
+		}
+		else {//종유석
+			update(H - h, H - 1);
+		}
 	}
+	int cnt, minVal = MAX_N, breaks;
+	for (int i = 0; i < H; i++) {
+		breaks = query(i);
+		if (minVal > breaks) {
+			minVal = breaks;
+			cnt = 1;
+		}
+		else if (minVal == breaks)cnt++;
+	}
+	cout << minVal << " " << cnt << "\n";
 }

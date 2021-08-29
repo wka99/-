292442c5d3f571a33cf203bd4bb50695747b9e2a
Soyoung Yoo <yooso020599@naver.com>
2021-08-29 23:10:15 +0900

    08/29

--- baekjoon/des/소스.cpp
+++ baekjoon/des/소스.cpp
@@ -1,30 +1,6 @@
 #include <iostream>
 using namespace std;
 
-long long x, y, z;
-
 int main() {
-	cin >> x >> y;
-	z = y * 100 / x;
-	if (z >= 99) { //소수점 이하를 버리기 때문에 더 이상 변할 수 없음
-		cout << -1 << endl;
-	}
-	else {
-		// y:980000000, x:1000000000 인 경우 최대 1000000000판을 더해야 z값을 1 올릴 수 있다.
-		int low = 0, high = 1000000000, mid;
-		long long score;
-		int ans = -1;
-		while (low <= high) {
-			mid = (low + high) / 2;
-			score = (y + mid) * 100 / (x + mid);
-			if (z >= score) {
-				//score가 z보다 작거나 같도록 해주는 마지노선에 존재하는 게임수에 1을
-				//더한 것이 승률을 높이기 위해 최소로 플레이해야 하는 게임의 수이다.
-				low = mid + 1;
-				ans = mid + 1;
-			}
-			else high = mid - 1;
-		}
-		cout << ans << endl;
-	}
+
 }
--- dataStructure/dataStructure/소스1.cpp
+++ dataStructure/dataStructure/소스1.cpp
@@ -1,52 +1,51 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <algorithm>
 using namespace std;
-#define MAX 1000
+#define INF 1000000000
 
-vector<vector<int>>v(100, vector<int>(100, 0));
-int visited[MAX] = { 0, };
+vector<int> list = { 5,3,8,1,2,7,9,4 };
+int N = list.size();
 
-void DFS(int e) {
-	visited[e] = 1;
-	cout << e << endl;
-	for (int i = 0; i < v[e].size(); i++) {
-		if (!visited[v[e][i]]) {
-			DFS(v[e][i]);
-		}
+/*
+	[선택 정렬]
+	정렬되지 않은 수 중에서 가장 작은 수를 찾아 정렬된 리스트의
+	뒤에 붙이는 과정을 정렬되지 않은 수가 없어질 때까지 반복
+
+	연산 횟수 (n-1)+(n-2)+...+1 따라서 시간 복잡도 O(n^2)
+*/
+void selectionSort() {
+	for (int i = 0; i < N-1; i++) {
+		int idx = i;
+		for (int j = i + 1; j < N; j++)
+			if (list[idx] > list[j])idx = j;
+		swap(list[idx], list[i]);
 	}
 }
-void BFS(int e) {
-	visited[e] = 1;
-	queue<int> q;
-	q.push(e);
-	int top;
-	while (!q.empty()) {
-		top = q.front();
-		cout << top << endl;
-		q.pop();
-		for (int i = 0; i < v[top].size(); i++) {
-			if (!visited[v[top][i]]) {
-				q.push(v[top][i]);
-				visited[v[top][i]] = 1;
-			}
+/*
+	[삽입 정렬]
+	정렬되지 않은 숫자를 올바른 위치에 삽입하는 과정을 반복해
+	리스트를 정렬하는 방법
+
+	연산 횟수 (n-1)+(n-2)+...+1 따라서 시간 복잡도 O(n^2)
+*/
+void insertionSort() {
+	for (int i = 1; i < N; i++) {
+		int idx = i - 1;
+		int key = list[i];
+		while (idx >= 0) {
+			if (key < list[idx])
+				list[idx + 1] = list[idx];
+			else break;
+			idx--;
 		}
+		list[idx + 1] = key;
 	}
 }
 int main() {
-	v[0].push_back(1);
-	v[0].push_back(2);
-	v[1].push_back(0);
-	v[2].push_back(0);
-	v[3].push_back(4);
-	v[4].push_back(3);
-	int cnt = 0;
-	for (int i = 0; i < 4; i++) {
-		if (!visited[i]) {
-			DFS(i);
-			cnt++;
-			cout << endl;
-		}
-	}
-	cout << cnt << endl;
+	for (int i = 0; i < list.size(); i++) cout << list[i] << " ";
+	cout << endl;
+	insertionSort();
+	for (int i = 0; i < list.size(); i++) cout << list[i] << " ";
+	cout << endl;
 }

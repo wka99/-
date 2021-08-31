e7ac44899d892a23a9251340cd2ff46528b7d1fd
Soyoung Yoo <yooso020599@naver.com>
2021-08-31 23:52:21 +0900

    08/31

--- dataStructure/dataStructure/소스1.cpp
+++ dataStructure/dataStructure/소스1.cpp
@@ -2,50 +2,97 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-#define INF 1000000000
 
-vector<int> list = { 5,3,8,1,2,7,9,4 };
+vector<int> list = { 2,1,4,9,8,3,6,7,5 };
 int N = list.size();
 
-/*
-	[선택 정렬]
-	정렬되지 않은 수 중에서 가장 작은 수를 찾아 정렬된 리스트의
-	뒤에 붙이는 과정을 정렬되지 않은 수가 없어질 때까지 반복
-
-	연산 횟수 (n-1)+(n-2)+...+1 따라서 시간 복잡도 O(n^2)
-*/
-void selectionSort() {
-	for (int i = 0; i < N-1; i++) {
-		int idx = i;
-		for (int j = i + 1; j < N; j++)
-			if (list[idx] > list[j])idx = j;
+void selectionSort() { //선택 정렬
+	for (int i = 0; i < N - 1; i++) {
+		int idx = i; //최솟값 위치
+		for (int j = i + 1; j < N; j++) {
+			if (list[idx] > list[j]) 
+				idx = j;
+		}
 		swap(list[idx], list[i]);
 	}
 }
-/*
-	[삽입 정렬]
-	정렬되지 않은 숫자를 올바른 위치에 삽입하는 과정을 반복해
-	리스트를 정렬하는 방법
-
-	연산 횟수 (n-1)+(n-2)+...+1 따라서 시간 복잡도 O(n^2)
-*/
-void insertionSort() {
+void insertionSort() { //삽입 정렬
 	for (int i = 1; i < N; i++) {
-		int idx = i - 1;
+		int j = i - 1;
 		int key = list[i];
-		while (idx >= 0) {
-			if (key < list[idx])
-				list[idx + 1] = list[idx];
+		while (j >= 0) {
+			if (key < list[j]) list[j + 1] = list[j];
 			else break;
-			idx--;
+			j--;
 		}
-		list[idx + 1] = key;
+		list[j + 1] = key;
+	}
+}
+void bubbleSort() {
+	for (int i = N - 1; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			if (list[j] > list[j + 1])
+				swap(list[j], list[j + 1]);
+		}
+	}
+}
+void merge(int left, int right, int mid) { //합병 정렬 
+	int i = left, j = mid + 1;
+	vector<int> sorted;
+	while (i <= mid && j <= right) {
+		if (list[i] < list[j]) {
+			sorted.push_back(list[i]);
+			i++;
+		}
+		else {
+			sorted.push_back(list[j]);
+			j++;
+		}
+	}
+	while (i <= mid) {
+		sorted.push_back(list[i]);
+		i++;
+	}
+	while (j <= right) {
+		sorted.push_back(list[j]);
+		j++;
+	}
+	for (int i = left; i <= right; i++) {
+		list[i] = sorted[i-left];
+	}
+}
+void mergeSort(int left, int right) {
+	int mid = (left + right) / 2;
+	if (left < right) {
+		mergeSort(left, mid);
+		mergeSort(mid + 1, right);
+		merge(left, right, mid);
+	}
+}
+int partition(int left, int right) { //퀵정렬
+	//피벗을 정하고 피벗을 기준으로 정렬 => 피벗의 위치 반환
+	int pivot = list[left];//첫번째 수를 피벗으로 정함
+	int low = left + 1, high = right;
+	while (low < high) {
+		while (low <= right && pivot <= list[low]) low++;
+		while (high >= left && pivot >= list[high]) high--;
+		if (low < high) swap(list[low], list[high]);
+	}
+	swap(list[left], list[high]);
+	return high;
+}
+void quickSort(int left, int right) {
+	if (left < right) {
+		int p_idx = partition(left, right); //피벗 위치 반환 => 피벗은 이미 정렬된 상태
+		//따라서 피벗을 제외한 나머지만 퀵소트 수행
+		quickSort(left, p_idx - 1);
+		quickSort(p_idx + 1, right);
 	}
 }
 int main() {
-	for (int i = 0; i < list.size(); i++) cout << list[i] << " ";
-	cout << endl;
-	insertionSort();
-	for (int i = 0; i < list.size(); i++) cout << list[i] << " ";
-	cout << endl;
+	for (int i = 0; i < N; i++) cout << list[i] << " ";
+	cout << "\n";
+	bubbleSort();
+	for (int i = 0; i < N; i++) cout << list[i] << " ";
+	cout << "\n";
 }
